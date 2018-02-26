6e6559a7cda8ec2f3581e512b0af5bf870029cea
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-02-26 20:12:01 +0300

    Update tabl1.cpp

--- tabl1.cpp
+++ tabl1.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    double sumkvadrat = 0.0;
     int n;
     cout << "Введите количество измерений" << endl;
     cin >> n;
@@ -20,7 +21,7 @@ int main()
     
     //Массив значений заполнен.
     
-    double sum = 0;
+    double sum = 0.0;
     for (int i = 1; i <= n; i++)
     {
        sum = sum + a[i]; 
@@ -28,9 +29,23 @@ int main()
     
     cout << "Сумма равна " << sum << endl;
     cout << endl;
-    double med;
+    double med = 0.0;
     med = sum / n;
     cout << "Среднее значение равно " << med << endl;
     cout << endl;
+    
+    for (int i=1; i<=n; i++)
+    {
+        cout << "Отклонение" << i << " равно " << a[i] - med << endl;
+    }
+    
+    for (int i=1; i<=n; i++)
+    {
+        cout << "Квадратичное Отклонение" << i << " равно " << pow ((a[i] - med), 2) << endl;
+        sumkvadrat += pow ((a[i] - med), 2);
+    }
+    
+    cout << "Сумма 3-его столбца равна " << sumkvadrat;
+    return 0;
     
 }
